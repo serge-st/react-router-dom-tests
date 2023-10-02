657a271cc6976b8457352d8c3eaf3eaf917590d6
Serge Stecenko <sergejs.stecenko@gmail.com>
2023-10-02 16:51:37 +0300

    feat: contextual error for destroy route

--- src/routes/destroy.ts
+++ src/routes/destroy.ts
@@ -2,6 +2,7 @@ import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts.js";
 
 export async function action({ params }) {
+    throw new Error("oh dang!");
     await deleteContact(params.contactId);
     return redirect("/");
 }
